a726b6dee30e317731ef04b10a9f11f31dc3a13e
Sarveshwaran_Rajaraman <wsarvesh19@outlook.com>
2026-04-02 09:13:17 -0400

    started CubicEquation

---
+++ vite-project/src/Components/CubicEquation.tsx
@@ -0,0 +1,10 @@
+type CubicEquation = {
+        a:number 
+        setA: (value: number) => void
+        b:number 
+        setB: (value: number) => void
+        c:number 
+        setC: (value: number) => void
+        d:number 
+        setD: (value: number) => void
+}
